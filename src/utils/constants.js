85bc52e8295f6693d95b82457611a63f0ed2f68e
AkhilAGilgal <30572889+AkhilAGilgal@users.noreply.github.com>
2025-06-14 18:17:53 +0530

    Create constants.js

---
+++ src/utils/constants.js
@@ -0,0 +1,14 @@
+import MusicNoteIcon from '@mui/icons-material/MusicNote';
+import HomeIcon from '@mui/icons-material/Home';
+
+
+export const logo = 'https://i.ibb.co/s9Qys2j/logo.png';
+
+export const categories = [
+  { name: 'New', icon: <HomeIcon />, },
+  { name: 'Music', icon: <MusicNoteIcon /> },
+
+];
+
+export const demoThumbnailUrl = 'https://i.ibb.co/G2L2Gwp/API-Course.png';
+export const demoChannelUrl = '/channel/UCmXmlB4-HJytD7wek0Uo97A';
